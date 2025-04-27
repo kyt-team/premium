90f0e323b2057c634acf247c5973fdc6375af992
kyt-team <153147214+kyt-team@users.noreply.github.com>
2025-04-27 16:51:50 +0700

    Create lamin.sh

---
+++ lamin.sh
@@ -0,0 +1,183 @@
+#!/bin/bash
+if [[ $(ulimit -c) != "0" ]]; then
+  echo "Saya Memantau Anda..."
+  echo "- @user_legend"
+  exit 1
+fi
+
+merah='\e[1;31m'
+hijau='\e[0;32m'
+NC='\e[0m'
+IP=$(curl -sS ipinfo.io/ip > /tmp/ipaddress.txt)
+MYIP=$(cat /tmp/ipaddress.txt)
+
+# cek wget & curl
+if ! which wget > /dev/null; then
+clear
+echo -e "${merah}Wah Mau Belajar Nakal Yah !${NC}"
+sleep 2
+exit 0
+clear
+else
+echo "Wget sudah terinstal"
+fi
+
+if ! which curl > /dev/null; then
+clear
+echo -e "${merah}Wah Mau Belajar Nakal Yah !${NC}"
+sleep 2
+exit 0
+clear
+else
+echo "curl sudah terinstal"
+fi
+
+fileee=/usr/bin/wget
+minimumsize=400000
+actualsize=$(wc -c <"$fileee")
+if [ $actualsize -ge $minimumsize ]; then
+clear
+echo -e "${hijau}Memeriksa...${NC}"
+else
+clear
+echo -e "${merah}Permission Denied!${NC}";
+echo "Alasan: Paket Dimodifikasi untuk Menghindari Script"
+exit 0
+fi
+
+fileeex=/usr/bin/curl
+minimumsizex=15000
+clear
+actualsizex=$(wc -c <"$fileeex")
+if [ $actualsizex -ge $minimumsizex ]; then
+clear
+echo -e "${hijau}Memeriksa...${NC}"
+else
+clear
+echo -e "${merah}Permission Denied!${NC}";
+echo "Alasan: Paket Dimodifikasi untuk Menghindari Script"
+exit 0
+fi
+
+# data server
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+cekcloudflare=$(curl -sS http://ip-api.com/json | jq .as | grep -o "Cloudflare")
+if [[ "$cekcloudflare" = "Cloudflare" ]]; then
+cekdomen=$(cat /etc/xray/domain)
+MYIP=$(dig +short "$cekdomen" | head -n 1)
+fi
+repogithub='kyt-team/premium/main'
+clear
+
+domain=$(cat /etc/xray/domain)
+pathku=$(cat /etc/xray/path/vmess_ws_path)
+ngecek=$(cat /etc/systemd/system/slowdns.service | grep -o "443")
+nsdomain=$(cat /etc/ns/domain);
+pubkey=$(cat /etc/william/slowdns/server.pub);
+tls="$(cat ~/log-install.txt | grep -w "TLS" | cut -d: -f2|sed 's/ //g')"
+none="$(cat ~/log-install.txt | grep -w "VMNONE" | cut -d: -f2|sed 's/ //g')"
+alternatifportntls=$(cat /etc/nginx/conf.d/vmnone.conf | grep listen | sed -n '2,$p' | awk '{printf $2 ", "; if(NR==FNR) printf ""}' | sed '$s/, $/./')
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+		read -rp "User: " -e user
+		CLIENT_EXISTS=$(grep -w $user /usr/local/etc/xray/config.json | wc -l)
+
+		if [[ ${CLIENT_EXISTS} == '1' ]]; then
+			echo ""
+			echo "Akun dengan nama tersebut sudah ada, silakan pilih nama lain."
+			exit 1
+		fi
+	done
+cekvalidasi=$(cat /usr/local/etc/xray/config.json | grep -i "$user" | awk {'print $2'} | head -1 | wc -l)
+if [ "$cekvalidasi" == "1" ]; then
+echo ""
+echo "user $user sudah ada"
+echo "silakan pilih nama lain"
+exit 1
+fi
+uuid="lamin-$user"
+read -p "Expired (hari): " masaaktif
+KEY=$(cat /etc/william/profile/key 2>/dev/null)
+CHATID=$(cat /etc/william/profile/key 2>/dev/null)
+if [[ -n $KEY || -n $CHATID ]]; then
+read -p "Batas IP Login (masukkan atau ketik 0 untuk tanpa batas): " limit_ip
+if [[ -z "$limit_ip" || "$limit_ip" == "0" ]]; then
+  limit_ip="0"
+fi
+echo "$limit_ip" > "/etc/william/limit-xray/vmessws/$user"
+fi
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+sed -i '/#vmessws$/a\### '"$user $exp VmessWS-TLS "'\
+,{"id": "'""$uuid""'","level": '"0"',"email": "'""$user""'"}' /usr/local/etc/xray/config.json
+sed -i '/#vmessWS$/a\### '"$user $exp VmessWS-NTLS "'\
+},{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /usr/local/etc/xray/none.json
+cat>/etc/xray/vmess/$user-tls.json<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "${domain}",
+      "port": "${tls}",
+      "id": "${uuid}",
+      "aid": "0",
+      "net": "ws",
+      "path": "${pathku}",
+      "type": "none",
+      "host": "${domain}",
+      "tls": "tls"
+}
+EOF
+cat>/etc/xray/vmess/$user-none.json<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "${domain}",
+      "port": "${none}",
+      "id": "${uuid}",
+      "aid": "0",
+      "net": "ws",
+      "path": "${pathku}",
+      "type": "none",
+      "host": "${domain}",
+      "tls": "none"
+}
+EOF
+vmess_base641=$( base64 -w 0 <<< $vmess_json1)
+vmesslink1="vmess://$(base64 -w 0 /etc/xray/vmess/$user-tls.json)"
+vmesslink2="vmess://$(base64 -w 0 /etc/xray/vmess/$user-none.json)"
+systemctl restart xray
+systemctl restart xray@none
+service cron restart
+clear
+echo -e ""
+echo -e "━━━━━━━━━━━━━━━━━━━━"
+echo -e "━━━[XRAY/VMESS_WS]━━━"
+echo -e "━━━━━━━━━━━━━━━━━━━━"
+echo -e "Remarks : ${user}"
+ceklimit_ip=$(cat /etc/william/limit-xray/vmessws/$user 2>/dev/null)
+if [[ -z $ceklimit_ip ]]; then
+echo -e "Limit IP : Tidak Aktif"
+else
+if [ "$ceklimit_ip" == "0" ]; then
+echo -e "Limit IP : Tanpa Batas IP Login"
+else
+echo -e "Limit IP : $limit_ip"
+fi
+fi
+echo -e "Alamat IP : ${MYIP}"
+echo -e "Domain : ${domain}"
+if [[ "$ngecek" = "443" ]]; then
+echo -e "DNS Domain : $nsdomain"
+echo -e "Pub key slowdns : $pubkey"
+fi
+echo -e "Port TLS : ${tls}"
+echo -e "Port NONE-TLS : ${none}"
+echo -e "Port Alternatif NONE-TLS : $alternatifportntls"
+echo -e "ID : ${uuid}"
+echo -e "Network : Websocket"
+echo -e "Websocket Path : ${pathku}"
+echo -e "━━━━━━━━━━━━━━━━━━━━"
+echo -e "LINK WS TLS : ${vmesslink1}"
+echo -e "━━━━━━━━━━━━━━━━━━━━"
+echo -e "LINK WS NONE-TLS : ${vmesslink2}"
+echo -e "━━━━━━━━━━━━━━━━━━━━"
+echo -e "EXPIRED ON : $exp"
